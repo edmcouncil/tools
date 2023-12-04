69a9abd9fbe6e41266b052ecb25a7bff8961cbd0
mereolog <pawel.garbacz@makolab.com>
2023-12-04 11:28:31 +0100

    uncomment run compare

--- compare/reasoning/run_reasoner_compare.py
+++ compare/reasoning/run_reasoner_compare.py
@@ -5,11 +5,11 @@ from compare.reasoning.ontology_commits_collector import collect_consolidated_on
 from compare.reasoning.reasoner_executor import run_reasoner_over_folder
 
 logging.basicConfig(format='%(levelname)s %(asctime)s %(message)s', level=logging.WARN, datefmt='%m/%d/%Y %I:%M:%S %p')
-# collect_consolidated_ontology_commits(
-#     repo_github_url='https://github.com/edmcouncil/idmp',
-#     repo_folder='./idmp_repo/',
-#     consolidated_ontologies_folder='./idmp_ontology_versions/',
-#     nearest_commit_ignore_span=1,
-#     merge_ontology_file='AboutIDMPDev-ReferenceIndividuals.rdf',
-#     first_commit_date=datetime.date(year=2023, month=6, day=1))
+collect_consolidated_ontology_commits(
+    repo_github_url='https://github.com/edmcouncil/idmp',
+    repo_folder='./idmp_repo/',
+    consolidated_ontologies_folder='./idmp_ontology_versions/',
+    nearest_commit_ignore_span=1,
+    merge_ontology_file='AboutIDMPDev-ReferenceIndividuals.rdf',
+    first_commit_date=datetime.date(year=2023, month=7, day=1))
 run_reasoner_over_folder(folder_path='./idmp_ontology_versions/')
